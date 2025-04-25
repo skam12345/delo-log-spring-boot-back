7deb0f86b17a9f6915933b8e34ac00b767b9f20f
skam12345 <skam1234561@gmail.com>
2025-04-26 00:09:06 +0900

    cors origin 수정

--- app/src/main/java/back/end/config/SecurityConfig.java
+++ app/src/main/java/back/end/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3002")  // 프론트엔드 도메인
+                        .allowedOrigins("http://localhost:3002, https://de-lo-log.site")  // 프론트엔드 도메인
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
