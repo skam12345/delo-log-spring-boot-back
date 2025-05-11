fd080f68cec9997523723803b21b7170acf08b78
skam12345 <skam1234561@gmail.com>
2025-05-11 11:54:09 +0900

    오류 수정

--- app/src/main/java/back/end/config/SecurityConfig.java
+++ app/src/main/java/back/end/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package back.end.config;
 
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@
+@ConditionalOnDefaultWebSecurity
 public class SecurityConfig {
 
     @Bean
