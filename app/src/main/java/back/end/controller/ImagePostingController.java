57af0969bcec567c02fbcc94aedad3f4b2963f02
skam12345 <skam1234561@gmail.com>
2025-05-27 17:55:43 +0900

    upload image 파라미터 방식 수정

--- app/src/main/java/back/end/controller/ImagePostingController.java
+++ app/src/main/java/back/end/controller/ImagePostingController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -55,7 +56,7 @@ public class ImagePostingController {
     }
 
     @PostMapping("/write/image-upload")
-    public ResponseEntity<Map<String, Object>> imageUpload(@RequestParam("creationIdx") String creationIdx, @RequestParam("files") MultipartFile[] files, @RequestParam("thumnail") MultipartFile thumbnail) throws IOException {
+    public ResponseEntity<Map<String, Object>> imageUpload(@RequestParam("creationIdx") String creationIdx, @RequestPart("files") MultipartFile[] files, @RequestPart("thumnail") MultipartFile thumbnail) throws IOException {
         ResponseEntity<Map<String, Object>> returnValue = null;
         Integer idx = Integer.parseInt(creationIdx);
         try {
